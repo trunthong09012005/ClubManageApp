f80bd07c879eff84d5c01758d8f1001c3e699fcb
trunthong09012005 <6451071075@st.utc2.edu.vn>
2025-11-23 23:53:47 +0700

    new

--- Login.cs
+++ Login.cs
@@ -9,10 +9,9 @@
     {
         public partial class Login : Form
         {
-            private string connectionString =
-                @"Data Source=DESKTOP-EJIGPN3;Initial Catalog=QL_APP_LSC;User ID=sa;Password=1234;TrustServerCertificate=True";
+        private string connectionString = @"Data Source=21AK22-COM;Initial Catalog=QL_CLB_LSC;User ID=sa;Password=912005;TrustServerCertificate=True";
 
-            public Login()
+        public Login()
             {
                 InitializeComponent();
 
